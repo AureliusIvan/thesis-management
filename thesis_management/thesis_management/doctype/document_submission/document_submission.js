a24d9134cec13f3ce66dea933cc6041b2f4645c6
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-08-05 01:36:17 +0700

    feature/add-document-submission-doctype

---
+++ thesis_management/thesis_management/doctype/document_submission/document_submission.js
@@ -0,0 +1,2 @@
+// Copyright (c) 2024, Aurelius Ivan Wijaya and contributors
+// For license information, please see license.txt
---
+++ thesis_management/thesis_management/doctype/document_submission/test_document_submission.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024, Aurelius Ivan Wijaya and Contributors
+# See license.txt
+
+import frappe
+import ed25519
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDocumentSubmission(FrappeTestCase):
+	pass
